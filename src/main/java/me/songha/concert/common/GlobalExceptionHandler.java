2320fc7df10adbced3c6cc2530953d253831863a
songhashin <shsha9292@gmail.com>
2024-11-16 00:31:46 +0900

    feat: ExceptionHandler 구성

---
+++ src/main/java/me/songha/concert/common/GlobalExceptionHandler.java
@@ -0,0 +1,15 @@
+package me.songha.concert.common;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+@RestControllerAdvice
+public class GlobalExceptionHandler {
+
+    @ExceptionHandler(NotFoundException.class)
+    public ResponseEntity<String> handleNotFound(NotFoundException ex) {
+        return ResponseEntity.status(HttpStatus.NOT_FOUND).body(ex.getMessage());
+    }
+}
