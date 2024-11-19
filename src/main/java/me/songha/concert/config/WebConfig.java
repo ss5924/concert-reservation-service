c41b26b2b99df91ba3fb700b2848171633fddc06
songhashin <shsha9292@gmail.com>
2024-11-20 01:02:44 +0900

    feat: HandlerMethodArgumentResolver 적용하여 userId 추출

---
+++ src/main/java/me/songha/concert/config/WebConfig.java
@@ -0,0 +1,22 @@
+package me.songha.concert.config;
+
+import me.songha.concert.common.UserArgumentResolver;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.method.support.HandlerMethodArgumentResolver;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+import java.util.List;
+
+@Configuration
+public class WebConfig implements WebMvcConfigurer {
+    private final UserArgumentResolver userArgumentResolver;
+
+    public WebConfig(UserArgumentResolver userArgumentResolver) {
+        this.userArgumentResolver = userArgumentResolver;
+    }
+
+    @Override
+    public void addArgumentResolvers(List<HandlerMethodArgumentResolver> resolvers) {
+        resolvers.add(userArgumentResolver);
+    }
+}
---
+++ src/main/java/me/songha/concert/user/CurrentUser.java
@@ -0,0 +1,11 @@
+package me.songha.concert.user;
+
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+
+@Target(ElementType.PARAMETER)
+@Retention(RetentionPolicy.RUNTIME)
+public @interface CurrentUser {
+}
